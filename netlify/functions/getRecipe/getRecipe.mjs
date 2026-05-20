b5844803b300867c686e1244b94dca049306744c
Anne-Hélène <ahbricet@gmail.com>
2026-05-20 10:54:53 +0200

    add getRecipe.mjs

--- netlify/functions/getRecipe/getRecipe.mjs
+++ netlify/functions/getRecipe/getRecipe.mjs
@@ -1,5 +1,5 @@
-export async function handler(event) {
-  const { ingredientsString } = JSON.parse(event.body);
+export default async (request) => {
+  const { ingredientsString } = await request.json();
 
   const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Always respond in the same language that the user uses to write their ingredients.
@@ -28,8 +28,10 @@ You are an assistant that receives a list of ingredients that a user has and sug
 
   const data = await response.json();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ recipe: data.choices[0].message.content }),
-  };
-}
+  return new Response(
+    JSON.stringify({ recipe: data.choices[0].message.content }),
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
+};
